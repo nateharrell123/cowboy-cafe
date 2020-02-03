767bd514f185febb8b7bcc2a554e86059b7c4c53
nateharrell <nateharrell@ksu.edu>
2020-02-03 16:47:49 -0600

    Chili Cheese Fries Class

---
+++ Data/ChiliCheeseFries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class ChiliCheeseFries : Side
+    {
+        public override uint Calories
+        {
+            get
+            {
+                switch(Size)
+                {
+                    case Size.Large:
+                        return 610;
+                    case Size.Medium:
+                        return 524;
+                    case Size.Small:
+                        return 433;
+                    default:
+                        throw new NotImplementedException("Unknown size");
+                }
+            }
+        }
+
+        public override double Price => throw new NotImplementedException();
+    }
+}
