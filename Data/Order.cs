47957ab9d7a3e9c0a9a08b91c771962a87abbaea
nateharrell <nateharrell@ksu.edu>
2020-02-26 17:05:13 -0600

    Added some fields

--- Data/Order.cs
+++ Data/Order.cs
@@ -8,12 +8,20 @@ namespace CowboyCafe.Data
     public class Order:INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged; // if any of these properties have changed, notify us. Subtotal Items, 
-        public double Subtotal => 0;
+        public double Subtotal { get; }
 
-        public IEnumerable<IOrderItem> Items => throw new NotImplementedException();
+        public IEnumerable<IOrderItem> Items { get; }
 
         public void Add(IOrderItem item) { }
 
         public void Remove(IOrderItem item) { }
+
+        private int lastOrderNumber;
+
+        public uint OrderNumber { get; }
+
+        private List<IOrderItem> items = new List<IOrderItem>();
+
+        
     }
 }
