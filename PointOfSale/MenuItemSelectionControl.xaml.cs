b1c5dca7c2a61599c4800abdf198ac873f7522cb
nateharrell <nateharrell@CIS-RDS3>
2020-03-01 17:16:22 -0600

    Almost finished Point of Sale Milestone 2

--- PointOfSale/MenuItemSelectionControl.xaml.cs
+++ PointOfSale/MenuItemSelectionControl.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CowboyCafe.Data;
 
 namespace PointOfSale
 {
@@ -21,6 +22,200 @@ namespace PointOfSale
         public MenuItemSelectionControl()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Creates new Cowpoke Chili object and adds it to the Order List Checked List Box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddCowPokeChiliButton(object sender, EventArgs e)
+        {
+            if(DataContext is Order order)
+            {
+                order.Add(new CowpokeChili());
+            }
+        }
+
+        /// <summary>
+        /// Creates new Angry Chicken object and adds it to the Order List Checked List Box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddAngryChickenButton(object sender, EventArgs e)
+        {
+            if (DataContext is Order order)
+            {
+                order.Add(new AngryChicken());
+            }
+        }
+
+        /// <summary>
+        /// Creates new Baked Beans object and adds it to the Order List Checked List Box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddBakedBeansButton(object sender, EventArgs e)
+        {
+            if (DataContext is Order order)
+            {
+                order.Add(new BakedBeans());
+            }
+        }
+
+        /// <summary>
+        /// Creates new Chili Cheese Fries object and adds it to the Order List Checked List Box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddChiliCheeseFriesButton(object sender, EventArgs e)
+        {
+            if (DataContext is Order order)
+            {
+                order.Add(new ChiliCheeseFries());
+            }
+        }
+
+        /// <summary>
+        /// Creates new Corn Dodgers object and adds it to the Order List Checked List Box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddCornDodgersButton(object sender, EventArgs e)
+        {
+            if (DataContext is Order order)
+            {
+                order.Add(new CornDodgers());
+            }
+        }
+
+        /// <summary>
+        /// Creates new Pan De Campo object and adds it to the Order List Checked List Box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddPanDeCampoButton(object sender, EventArgs e)
+        {
+            if (DataContext is Order order)
+            {
+                order.Add(new PanDeCampo());
+            }
+        }
+
+        /// <summary>
+        /// Creates a new Cowboy Coffee object and adds it to the Order List Checked List Box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddCowBoyCoffeeButton(object sender, EventArgs e)
+        {
+            if (DataContext is Order order)
+            {
+                order.Add(new CowboyCoffee());
+            }
+        }
+
+        /// <summary>
+        /// Creates a new Jerked Soda object and adds it to the Order List Checked List Box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddJerkedSodaButton(object sender, EventArgs e)
+        {
+            if (DataContext is Order order)
+            {
+                order.Add(new JerkedSoda());
+            }
+        }
+
+        /// <summary>
+        ///  Creates a new Texas Tea object and adds it to the Order List Checked List Box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddTexasTeaButton(object sender, EventArgs e)
+        {
+            if (DataContext is Order order)
+            {
+                order.Add(new TexasTea());
+            }
+        }
+        /// <summary>
+        ///  Creates a new Water object and adds it to the Order List Checked List Box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddWaterButton(object sender, EventArgs e)
+        {
+            if (DataContext is Order order)
+            {
+                order.Add(new Water());
+            }
+        }
+
+        /// <summary>
+        ///  Creates a new Dakota Double Burger object and adds it to the Order List Checked List Box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddDakotaDoubleBurgerButton(object sender, EventArgs e)
+        {
+            if (DataContext is Order order)
+            {
+                order.Add(new DakotaDoubleBurger());
+            }
+        }
+
+        /// <summary>
+        ///  Creates a new Pecos Pulled Pork object and adds it to the Order List Checked List Box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddPecosPulledPorkButton(object sender, EventArgs e)
+        {
+            if (DataContext is Order order)
+            {
+                order.Add(new PecosPulledPork());
+            }
+        }
+
+        /// <summary>
+        ///  Creates a new Rustler's Ribs object and adds it to the Order List Checked List Box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddRustlersRibsButton(object sender, EventArgs e)
+        {
+            if (DataContext is Order order)
+            {
+                order.Add(new RustlersRibs());
+            }
+        }
+
+        /// <summary>
+        ///  Creates a new Texas Triple Burger object and adds it to the Order List Checked List Box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddTexasTripleBurgerButton(object sender, EventArgs e)
+        {
+            if (DataContext is Order order)
+            {
+                order.Add(new TexasTripleBurger());
+            }
+        }
+
+        /// <summary>
+        ///  Creates a new Trail Burger object and adds it to the Order List Checked List Box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddTrailBurgerButton(object sender, EventArgs e)
+        {
+            if (DataContext is Order order)
+            {
+                order.Add(new TrailBurger());
+            }
         }
     }
 }
