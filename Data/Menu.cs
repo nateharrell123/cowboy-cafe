8ae5c878626f9b51b12658fad4a38e16bfb5721e
nateharrell <nateharrell@CIS-RDS4>
2020-04-19 13:41:58 -0500

    stuff

---
+++ Data/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    static class Menu
+    {
+        static IEnumerable<IOrderItem> Entrees()
+        {
+            var chili = new CowpokeChili();
+            var dakotaDoubleBurger = new DakotaDoubleBurger();
+            var pecosPulledPork = new PecosPulledPork();
+            var rustlersRibs = new RustlersRibs();
+            var texasTripleBurger = new TexasTripleBurger();
+            var trailBurger = new TrailBurger();
+
+        }
+    }
+}
